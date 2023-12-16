7cd1e6f9e713724af1faea329534279905e80103
Rizalfauzi13 <muhammadrizalfauzi@gmail.com>
2023-12-16 18:53:15 +0700

    first commit

---
+++ parkir.js
@@ -0,0 +1,26 @@
+function hitungParkir(){
+    // dapatkan nilai inputtan user
+    let masuk = document.getElementById('masuk').value
+    let keluar = document.getElementById('keluar').value
+
+    // dapatkan elemen untuk menampilkan hasil 
+    let hasilDurasi =document.querySelector("#hasilDurasi")
+    let hasilBiaya = document.querySelector("#hasilBiaya")
+
+    // hasl dirasi
+    let durasi = keluar - masuk;
+
+    // hitung biaya 
+    let biaya = 3000;
+
+
+    // setelah 2 jam pertama , tambahkan biaya perjamnya 1000
+    if( durasi > 2){
+        biaya += (durasi- 2) *1000
+    }
+    console.log(biaya);
+
+
+    hasilBiaya.innerHTML = biaya
+    hasilDurasi.innerHTML = durasi
+}
